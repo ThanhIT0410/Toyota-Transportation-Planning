5a8597812257be7bb9c092f6242d1d493a49e6d5
ThanhIT0410 <trinhthanhit104@gmail.com>
2025-02-09 02:47:29 +0700

    Initial commit

---
+++ src/main/java/org/toyotatransportationplanning/ToyotaTransportationPlanningApplication.java
@@ -0,0 +1,13 @@
+package org.toyotatransportationplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyotaTransportationPlanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyotaTransportationPlanningApplication.class, args);
+    }
+
+}
